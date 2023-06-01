e6b51f37a4d3a85621ffbb8c7d014bebaef40854
ShazSeatter <shazseatter12@gmail.com>
2023-06-01 22:30:33 +0100

    hero page editing

--- src/container/Heropage.jsx
+++ src/container/Heropage.jsx
@@ -4,12 +4,15 @@ import './Heropage.css'
 
 const Heropage = () => {
     return (
+        <>
         <div classname="relative h-32">
             <div className="absolute inset-0 flex flex-col items-center justify-center bg-black">
                 <h1 className="md:text-[4rem]/[4.5rem] text-[2.5rem] text-color">Shazreen Seatter</h1>
                 <h2 className="md:text-[2.5rem]/[4.5rem] text-[1.5rem] text-color">Junior Software Developer.</h2>
             </div>
         </div>
+
+      </>
     )
 }
 
