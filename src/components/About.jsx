72013b782df1ad2d5e360535813d22f42e6a2d16
ShazSeatter <shazseatter12@gmail.com>
2023-06-03 14:48:35 +0100

    working on scroll

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import "./About.css"
+import "../App.css";
+import "./About.css"; 
 
 const About = () => {
 
@@ -7,8 +8,8 @@ const About = () => {
         <>
         <section id="about-me" className="bg-black h-screen">
             <div className="card-container">
-                <h3 className="about-section-title text-lg">About me</h3>
-                <p className="p-text-color">Hello there! I'm Shaz and thank you for visiting my site! I'm a junior software developer having recently graduated from CodeClan's 16-week intensive Professional Software development course. I have a background in healthcare, having previously worked as a Physiotherpist. I changed careers as I love the problem solving aspect of programming and the creavity you can bring to the projects you create. During my time at CodeClan I experienced over 800 hours of hands-on coding experience, learning the fundamentals of development in Python, Javascript and Java. I am eager to expand on my newfound knowledge as I enter the tech industry. 
+                <h3 className="text-color main-heading md:text-[3.5rem]/[4rem] text-[2rem]">About me</h3>
+                <p className="p-text-color text-color sub-text md:text-[1rem]/[2rem] text-[1rem] mt-[2rem]">Hello there! I'm Shaz and thank you for visiting my site! I'm a junior software developer having recently graduated from CodeClan's 16-week intensive Professional Software development course. I have a background in healthcare, having previously worked as a Physiotherpist. I changed careers as I love the problem solving aspect of programming and the creavity you can bring to the projects you create. During my time at CodeClan I experienced over 800 hours of hands-on coding experience, learning the fundamentals of development in Python, Javascript and Java. I am eager to expand on my newfound knowledge as I enter the tech industry. 
                 </p>
             </div>
         </section>
--- src/container/Heropage.jsx
+++ src/container/Heropage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 import About from '../components/About';
-import './Heropage.css'
+import "../App.css";
+import './Heropage.css';
 
 
 const Heropage = () => {
@@ -17,8 +18,8 @@ const Heropage = () => {
         <>
         <div className="relative h-screen">
             <div className="absolute inset-0 flex flex-col items-center justify-center bg-black">
-                <h1 className="md:text-[4rem]/[4.5rem] text-[2.5rem] text-color">Shazreen Seatter</h1>
-                <h2 className="md:text-[2rem]/[4.5rem] text-[1.5rem] text-color">Junior Software Developer</h2>
+                <h1 className="md:text-[4rem]/[4.5rem] text-[2.5rem] main-heading text-color">Shazreen Seatter</h1>
+                <h2 className="md:text-[2rem]/[4.5rem] text-[1.5rem] sub-text text-color">Junior Software Developer</h2>
                 <button onClick={handleScroll}><div className="scroll"></div></button> 
             </div>
             </div>
